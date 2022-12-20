d512cc731e3775dce56c4fa6eb603c58d284a7b6
Yaroslav Drohoziuk <exmahinator@ukr.net>
2022-12-20 18:04:24 +0200

    trying to minimize avatar

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const fs = require("fs/promises");
+// const jimp = require("jimp");
 
 const { User } = require("../../models/user");
 
@@ -8,6 +9,8 @@ const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
   const { path: tempUpload, originalname } = req.file;
+  console.log("originalname:", originalname);
+  //   jimp.read(originalname);
   const filename = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, filename);
   await fs.rename(tempUpload, resultUpload);
