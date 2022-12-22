29dc647be2af78b4d0fd75b11abe202e648183f3
Yaroslav Drohoziuk <exmahinator@ukr.net>
2022-12-22 16:12:29 +0200

    hw05 - done... I think...

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,26 +1,20 @@
 const path = require("path");
 const fs = require("fs/promises");
-// const Jimp = require("jimp");
+const Jimp = require("jimp");
 
 const { User } = require("../../models/user");
-// const { minimizeAvatar } = require("../../helpers/minimizeAvatar");
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
   const { path: tempUpload, originalname } = req.file;
-  console.log("tempUpload:", tempUpload);
-  console.log("originalname:", originalname);
 
-  // minimizeAvatar(tempUpload, originalname);
+  const resizeImg = await Jimp.read(tempUpload);
+  resizeImg.resize(250, 250);
+  await resizeImg.writeAsync(tempUpload);
 
-  // const image = await Jimp.read(tempUpload);
-  // image.resize(250, 250);
-  // image.writeAsync(`mini_${originalname}`);
+  const filename = `${_id}_mini_${originalname}`;
 
-  // console.log("miniAvatar:", image);
-
-  const filename = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, filename);
   await fs.rename(tempUpload, resultUpload);
   const avatarURL = path.join("avatars", filename);
--- middlewares/upload.js
+++ middlewares/upload.js
@@ -8,6 +8,7 @@ const multerConfig = multer.diskStorage({
   filename: (req, file, cb) => {
     cb(null, file.originalname);
   },
+  limits: { fileSize: 1048576 },
 });
 
 const upload = multer({
